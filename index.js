4e27311bed02499f87878b106bffa35e9e0c31d2
Anthony <aamay022@fiu.edu>
2021-09-08 14:19:01 -0400

    tasks completed

--- index.js
+++ index.js
@@ -45,10 +45,11 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(arr){
+  arr.push();
+  return arr;
 }    
-
+console.log(copy(originalFlavors))
 
 
 
@@ -64,8 +65,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(arr){
+ if (arr.length === 31){
+   return true;
+ } else {
+   return false;
+ }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -81,8 +86,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(arr, newFlavor){
+ arr.unshift(newFlavor);
+ return arr;
 }
 
 
@@ -97,8 +103,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(arr){
+ arr.pop();
+ return arr;
 }
 
 
@@ -114,8 +121,8 @@ Use the getFlavorByIndex function below to do the following:
   For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+function getFlavorByIndex(arr, num){
+  return arr[num]
 }
 
 
@@ -134,9 +141,15 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(arr, flavor){
+  for (let i=0; i <= arr.length; i++){
+  if(arr[i]===flavor){
+    arr.splice(i, 1)
+    return arr;
+  }
 }
+}
+console.log(removeFlavorByName(originalFlavors, "Rocky Road"));
 
 
 
@@ -160,10 +173,16 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(arr, flavor){
+    const newArr = []
+    for (let i = 0 ; i<arr.length ; i++){
+    if (arr[i].includes(flavor)){
+    newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
-
+console.log(filterByWord(originalFlavors, "Chocolate"))
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
 
